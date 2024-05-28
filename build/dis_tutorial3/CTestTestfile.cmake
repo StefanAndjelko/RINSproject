e2a2037c7171ae5f8d28cfe05ba739c074ece29a
StefanAndjelko <120342911+StefanAndjelko@users.noreply.github.com>
2024-05-28 19:17:27 +0200

    Delete build directory

--- build/dis_tutorial3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mu/Documents/RINS/project/RINSproject/src/dis_tutorial3
-# Build directory: /home/mu/Documents/RINS/project/RINSproject/build/dis_tutorial3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/dis_tutorial6/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mu/Documents/RINS/project/RINSproject/src/dis_tutorial6
-# Build directory: /home/mu/Documents/RINS/project/RINSproject/build/dis_tutorial6
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
